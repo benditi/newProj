c2c1dd5ea1b3bb39c7e388a4fbc70472a32ea522
Ori <orisharonn@gmail.com>
2021-10-19 17:22:02 +0300

    changes

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const expressSession = require('express-session')
 const commentService = require('./service/comment.service')
 const app = express()
 const http = require('http').createServer(app)
-var gravatar = require('gravatar');
+// var gravatar = require('gravatar');
 // Express App Config
 const session = expressSession({
     secret: 'coding is amazing',
@@ -26,43 +26,35 @@ if (process.env.NODE_ENV === 'production') {
     app.use(cors(corsOptions))
 }
 
-
-
-// Make every server-side-route to match the index.html
-// so when requesting http://localhost:3030/index.html/car/123 it will still respond with
-// our SPA (single page app) (the index.html file) and allow vue/react-router to take it from there
-
-
 const port = process.env.PORT || 3030
 http.listen(port, () => {
 })
 
 //List
 app.get('/api/comment', (req, res) => {
-    commentService.query()
+    console.log('here');
+    console.log('params', req.query.filterBy);
+    commentService.query(req.query.filterBy)
         .then((comments) => {
+            console.log('comments are', comments);
             res.send(comments)
         })
 })
 
 //Create
-app.post('/api/comment', (req, res) => {
-    const comment = {
-        email: req.body.email,
-        imgUrl: req.body.imgUrl,
-        txt: req.body.txt
+app.post('/api/comment', async (req, res) => {
+    const comment = req.body.comment
+    // console.log('comment is', comment);
+    try {
+        const saved = await commentService.add(comment);
+        res.send(saved)
     }
-    console.log('req.body', req.body)
-    commentService.add(comment)
-        .then(savedComment => {
-            res.send(savedComment)
-        })
-        .catch(err => console.log(err))
+    catch (e) { console.log('e') }
 })
 
 
-const email = ( "orisharon@shemail.com " ).trim().toLowerCase();
-var url = gravatar.url(email);
-console.log(url);
+// const email = ("orisharon@shemail.com ").trim().toLowerCase();
+// var url = gravatar.url(email);
+// console.log(url);
 
 
--- service/comment.service.js
+++ service/comment.service.js
@@ -1,22 +1,34 @@
 const dbService = require('./db.service')
 const ObjectId = require('mongodb').ObjectId
+var md5 = require('md5');
 
-async function query(filterBy) {
+async function query(filterBy = '') {
     try {
-        // const criteria = _buildCriteria(filterBy)
-        // console.log('criteria', criteria);
+
         const collection = await dbService.getCollection('comment')
-        var toys = await collection.find().toArray()
-        return toys
+        var comments = await collection.find({ txt: { $regex: filterBy, $options: 'i' } }).toArray()
+        // console.log('comments', comments);
+        return comments
     } catch (err) {
         throw err
     }
 }
 async function add(comment) {
+    // console.log('comment', comment);
     try {
         const collection = await dbService.getCollection('comment')
-        const addedComment = await collection.insertOne(comment)
-        return addedComment
+        const found = await collection.findOne({ email: comment.email });
+        if (found) {
+            comment.imgUrl = found.imgUrl
+        }
+        else {
+            // comment.imgUrl = `https://www.gravatar.com/${md5(comment.email)}`
+            // console.log(comment.imgUrl);
+            comment.imgUrl = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYH_VDaGfxQ_cPhkgDPyoxXJgnnKHzEw7kdg&usqp=CAU'
+        }
+        await collection.insertOne(comment)
+        console.log('comment backend', comment);
+        return comment;
     } catch (err) {
         throw err
     }
